515174bc0b83fafcdcc344dfe4946e73951c3d9a
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2020-08-21 14:17:23 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function marinaraSauce(cookName) {
+    let two = 2;
+    let oneFourth = "1/4";
+    
+    console.log("Hey " + cookName + "! This is my marinara sauce recipe!");
+    console.log("- Heat a medium-large saucepan over medium heat.");
+    console.log("- Add " + two + " tsps of oil and 5 garlic cloves, cook until golden, about 2.");
+    console.log("- Add " + oneFourth + " cup of water, " + two + " cans of crushed tomatoes, and salt. Season with black pepper to taste.");
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes.");
+    console.log("- Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed.");
+}
+
+marinaraSauce("Cookie Monster");
